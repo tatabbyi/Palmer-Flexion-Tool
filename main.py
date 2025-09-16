7e4253c0fb01a53fb6c45af95f6a2c0ff90d42c8
tatabbyi <ytrachel0@gmail.com>
2025-09-16 15:52:19 +0100

    Images for cnn Preperation

--- main.py
+++ main.py
@@ -7,4 +7,10 @@ from utils import show-image
 model = tf.keras.models.load_model("palm_model.h5")
 class_names = ["normal", "simian", "sydney"]
 user_image = "images/text_user.jpg"
+result_img = detect_creases(user_image)
+show_image(result_img, "Detected Creases")
+
+img = image.load_img(user_image, target_size=(128,128))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0) / 255.0
 
