a1fb8c802b5b76a022d8e9d4840c64bab4b96c27
tatabbyi <ytrachel0@gmail.com>
2025-09-17 17:06:59 +0100

    Image Sizing

--- utils.py
+++ utils.py
@@ -10,9 +10,16 @@ def load_data(data_dir, img_size=(128,128), batch_size=32):
     class_names = dataset.class_names
     print("Loaded classes:", class_names)
 
-    dataset = dataset.map(lamda x, y: (x/255.0, y))
+    dataset = dataset.map(lambda x, y: (x/255.0, y))
 
     return dataset, class_names
 
 import cv2
 import numpy as np
+
+def preprocess_image(frame):
+
+    img = cv2.resize(frame, (128,128))
+    img = img / 255.0
+    img = np.expand_dims(img, axis=0)
+    return img 
