c8fabb112fd87ad63dcb7a811c01cbe99bdb813a
tatabbyi <ytrachel0@gmail.com>
2025-09-30 17:51:14 +0100

    Working Loop and fixes

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 import numpy as np
-from detect import detect_creases
-from utils import show-image
+
 
 model = tf.keras.models.load_model("palm_model.h5")
-class_names = ["normal", "simian", "sydney"]
-user_image = "images/text_user.jpg"
-result_img = detect_creases(user_image)
-show_image(result_img, "Detected Creases")
+class_names = ["simian", "sydney"]
+user_image = "images/simian/simian-01.jpg"
+
 
 img = image.load_img(user_image, target_size=(128,128))
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0) / 255.0
 
-pred = model.predict(X)
+pred = model.predict(x)
 predicted_class = class_names[np.argmax(pred)]
 print(f"Predicted Class: {predicted_class}")
 
